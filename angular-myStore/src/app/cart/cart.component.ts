2d7869bf6d431578fc5a8dcc61fcf66eb1937f1b
vitor <w.wutaes@gmail.com>
2022-06-26 09:31:34 -0300

    formulário de compras

--- angular-myStore/src/app/cart/cart.component.ts
+++ angular-myStore/src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { CartService } from './../cart.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -8,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor(private cartService: CartService) { }
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+    ) { }
   
   items = this.cartService.getItems()
   
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  })
+
+  onSubmit(): void {
+    // process checkout data here
+    this.items = this.cartService.clearCart()
+    console.warn('your order has been submitted', this.checkoutForm.value)
+    this.checkoutForm.reset();
+  }
+
   ngOnInit(): void {
   }
 
